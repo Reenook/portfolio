ede4b73ebd36f06d29b899f6896adf84c61f8508
Reenook <reenadk101@gmail.com>
2024-09-22 14:29:45 +0500

    commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,6 +16,7 @@ const Contact = () => {
                 Get in Touch
             </motion.h2>
             <div className={"text-center tracking-tighter"}>
+
                 <motion.a
                     initial={{ opacity: 0, y: 20 }}
                     whileInView={{ opacity: 1, y: 0 }}
